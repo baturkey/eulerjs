4b8efe4799c6f9a72d84ac25560aad782de6eddf
Jeffrey Liu <jeffrey@ipro.us>
2016-11-18 11:45:42 -0500

    Speed up 3

--- euler_003.js
+++ euler_003.js
@@ -5,33 +5,20 @@
 // 
 // What is the largest prime factor of the number 600851475143?
 
-var isPrime = function() {
-	var memo = [false, false, true];
-
-	return function(n) {
-		if(n in memo) {
-			return memo[n];
+function isOddPrime(n) {
+	for(var i = 3; i <= Math.sqrt(n); i += 2) {
+		if(n % i == 0) {
+			return false;
 		}
-
-		if(n % 2 == 0) {
-			return (memo[n] = false);
-		}
-	
-		for(var i = 3; i <= Math.sqrt(n); i += 2) {
-			if(n % i == 0) {
-				return (memo[n] = false);
-			}
-		}
-		return (memo[n] = true);
-	};
-}();
+	}
+	return true;
+}
 
 var max = 0;
 var n = 600851475143;
 for(var i = 3; i < Math.sqrt(n); i += 2) {
-	if(n % i == 0 && isPrime(i)) {
+	if(n % i == 0 && isOddPrime(i)) {
 		max = i;
 	}
 }
-
 return max;
