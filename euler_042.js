37eed65b3eece4323a12e55616d569f262532271
Jeffrey Liu <jeffrey@ipro.us>
2016-11-18 11:24:17 -0500

    Add solution to 42

---
+++ euler_042.js
@@ -0,0 +1,27 @@
+// Problem 42
+// ==========
+// 
+// The n-th term of the sequence of triangle numbers is given by, t[n] =
+// 1/2n(n+1); so the first ten triangle numbers are:
+// 
+//                  1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
+// 
+// By converting each letter in a word to a number corresponding to its
+// alphabetical position and adding these values we form a word value. For
+// example, the word value for SKY is 19 + 11 + 25 = 55 = t[10]. If the word
+// value is a triangle number then we shall call the word a triangle word.
+// 
+// Using words.txt, a 16K text file containing nearly two-thousand common
+// English words, how many are triangle words?
+
+var fs   = require('fs');
+var path = require('path');
+
+return fs.readFileSync(path.join('.', 'words.txt'), {encoding: 'utf-8'})
+	.split(",")
+	.map(x => x.replace(/"/g, "")
+		 .split('')
+		 .map(x => x.charCodeAt() - 64)
+		 .reduce((a, b) => a + b))
+	.filter(x => Number.isInteger(Math.sqrt(8*x + 1)))
+	.length;
