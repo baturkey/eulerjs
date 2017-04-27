134abefae51e220ade964bac5e1f173c531ef9f6
Jeffrey Liu <jeffrey@ipro.us>
2017-04-27 15:51:09 -0400

    Speed up 36

--- euler_036.js
+++ euler_036.js
@@ -10,31 +10,18 @@
 // (Please note that the palindromic number, in either base, may not include
 // leading zeros.)
 
-function convert(n) {
+function convert(n, base = 10) {
 	var output = [];
 	while(n > 0) {
-		output.unshift(n % 10);
-		n = Math.floor(n / 10);
+		output.unshift(n % base);
+		n = Math.floor(n / base);
 	}
 	return output;
 }
 
-function dec2bin(n) {
-	var max2 = Math.floor(Math.log2(n));
-	var b_a = Array(max2 + 1).fill(0);
-
-	while(n > 0) {
-		b_a[max2] = 1;
-		n -= Math.pow(2, max2);
-		max2 = Math.floor(Math.log2(n));
-	}
-	return b_a;
-}
-
 function isPalindrome(a) {
-	var l = a.length;
-	for(var i = 0; i < l / 2; i++) {
-		if(a[i] != a[l - 1 - i]) {
+	for(var i = 0; i < a.length / 2; i++) {
+		if(a[i] != a[a.length - 1 - i]) {
 			return false;
 		}
 	}
@@ -42,7 +29,7 @@ function isPalindrome(a) {
 }
 
 for(var sum = 0, i = 1; i < 1000000; i++) {
-	if(isPalindrome(convert(i)) && isPalindrome(dec2bin(i))) {
+	if(isPalindrome(convert(i)) && isPalindrome(convert(i, 2))) {
 		sum += i;
 	}
 }
