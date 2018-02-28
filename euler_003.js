58b00f641e6547c9743093f276cf97511fcc0a2c
Jeffrey Liu <jeff.liu@baublebar.com>
2018-02-28 13:21:41 -0500

    Speedup

--- euler_003.js
+++ euler_003.js
@@ -5,20 +5,22 @@
 // 
 // What is the largest prime factor of the number 600851475143?
 
-function isOddPrime(n) {
-	for(var i = 3; i <= Math.sqrt(n); i += 2) {
-		if(n % i == 0) {
-			return false;
-		}
+function isOddPrime(n)
+{
+    for (var i = 3; i <= Math.sqrt(n); i += 2) {
+	if (n % i == 0) {
+	    return false;
 	}
-	return true;
+    }
+    return true;
 }
 
-var max = 0;
-var n = 600851475143;
-for(var i = 3; i < Math.sqrt(n); i += 2) {
+const n = 0b1000101111100101100010011110101011000111;
+
+for(var i = Math.ceil(Math.sqrt(n)); i > 1; i -= 2) {
 	if(n % i == 0 && isOddPrime(i)) {
-		max = i;
+	    return i;
 	}
 }
-return max;
+
+return 0;
