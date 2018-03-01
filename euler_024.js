85262cd57ad69e0ab686a9da4eabf21409afeacc
Jeffrey Liu <jeff.liu@baublebar.com>
2018-03-01 13:28:55 -0500

    clean up

--- euler_024.js
+++ euler_024.js
@@ -12,17 +12,16 @@
 // 4, 5, 6, 7, 8 and 9?
 
 function permute(s) {
-	if(s.length == 1) {
-		return [s];
+    if(s.length == 1) {
+	return [s];
+    }
+    const output = [];
+    for(let e of permute(s.substr(1))) {
+	for(let pos = 0; pos <= e.length; pos++) {
+	    output.push(e.substr(0, pos) + s[0] + e.substr(pos));
 	}
-	var output = [];
-	var rest   = permute(s.substr(1));
-	for(var i in rest) {
-		for(var pos = 0; pos <= rest[i].length; pos++) {
-			output.push(rest[i].substr(0, pos) + s[0] + rest[i].substr(pos));
-		}
-	}
-	return output;
+    }
+    return output;
 }
 
 return permute("0123456789").sort()[999999];
